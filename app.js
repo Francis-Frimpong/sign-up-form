58c1030378517cf670b5bcf0c55210f230408207
Francis <brakay1136@gmail.com>
2025-03-10 12:15:43 -0700

    improved styling

---
+++ app.js
@@ -0,0 +1,5 @@
+//check if all input fields are empty
+//if user clicks on submit button while a field or all fields are empty
+// display error message
+//prevent form from submitting
+//submit form only if all field are filled correctly
