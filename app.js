41b79037abc349beac49a9fdd302a7218278516e
Francis <brakay1136@gmail.com>
2025-03-11 11:06:48 -0700

    adding Javascript functionality

--- app.js
+++ app.js
@@ -3,3 +3,27 @@
 // display error message
 //prevent form from submitting
 //submit form only if all field are filled correctly
+
+const inputs = document.querySelectorAll("input")
+const errorMsg = document.querySelectorAll('h6')
+let isEmpty = false;
+
+const formValidation = (input, errorMsg, isEmpty, event) => {
+    if (input.value.trim() === ""){
+        isEmpty = true;
+        input.style.border = "1px solid #ff7a7a";
+        errorMsg.forEach(message => {
+            message.style.display = 'block';
+        })
+    }
+
+    if(isEmpty){
+        event.preventDefault()
+    }
+}
+
+
+document.querySelector("form").addEventListener("submit", (event) => {
+    inputs.forEach(input => formValidation(input, errorMsg, isEmpty, event));
+
+})
